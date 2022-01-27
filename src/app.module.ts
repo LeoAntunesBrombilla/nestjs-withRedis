7bda0ee1780e882124b79c2c4bc42a41f96fef5a
LeoAntunesBrombilla <antunes.b.leonardo@gmail.com>
2022-01-27 14:21:59 -0300

    feat: trying to connect with redis

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,9 @@
 import { Module } from '@nestjs/common';
 import { UsersModule } from './users/users.module';
+import Redis from 'ioredis';
 
 @Module({
-  imports: [UsersModule],
+  imports: [Redis, UsersModule],
   controllers: [],
   providers: [],
 })
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,17 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { Transport, MicroserviceOptions } from '@nestjs/microservices';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  const app = await NestFactory.createMicroservice<MicroserviceOptions>(
+    AppModule,
+    {
+      transport: Transport.REDIS,
+      options: {
+        url: 'redis://localhost:6379',
+      },
+    },
+  );
+  app.listen();
 }
 bootstrap();
